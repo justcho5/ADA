bf10f590f061f7abca81c384ecce93d7e76965d6
Hyunjii Cho <justcho5@gmail.com>
2017-11-26 10:04:10 +0100

    script for transforming to parquet

--- project/compactify.py
+++ project/compactify.py
@@ -9,11 +9,10 @@ sqlContext = SQLContext(sc)
 
 # read the input file line by line
 #sampled_text.saveAsTextFile('hdfs:///user/cyu/sample0.01.json')
-text_file = sc.textFile('hdfs:///user/cyu/sample_cl_0.01.json/part-00000')
-#text_file = sc.textFile('hdfs:///datasets/productGraph/complete.json')
+# text_file = sc.textFile('hdfs:///user/hcho/part-00000')
+text_file = sc.textFile('hdfs:///datasets/productGraph/complete.json')
 
-
-reviews = text_file.map(lambda line: json.loads(line)) \
-    .reduce(lambda a, b: a + b)
-
-reviews.write.parquet('hdfs:///user/cyu/part-00000.parquet')
+reviews = text_file.map(lambda line: json.loads(line))
+df = sqlContext.createDataFrame(reviews)
+df.write.parquet('hdfs:///user/hcho/complete.parquet')
+# df.write.parquet('hdfs:///user/hcho/part-00000.parquet')
