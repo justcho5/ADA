6d5d83085e02c9c3997c7c66cb38da596a385245
Matteo Rizzo <32157960+matrizzo@users.noreply.github.com>
2017-12-07 22:19:31 +0100

    Update metadata preprocessing script

--- project/cluster_scripts/merge_reviews_metadata.py
+++ project/cluster_scripts/merge_reviews_metadata.py
@@ -7,7 +7,7 @@ JOINED_PATH = 'hdfs:///user/mrizzo/joined_df'
 sc = SparkContext()
 
 sqlContext = SQLContext(sc)
-sqlContext.setConf('spark.sql.parquet.compression.codec','snappy')
+sqlContext.setConf('spark.sql.parquet.compression.codec', 'snappy')
 
 reviews = sqlContext.read.parquet(REVIEW_PATH)
 meta = sqlContext.read.parquet(META_PATH)
--- project/cluster_scripts/preprocess_metadata.py
+++ project/cluster_scripts/preprocess_metadata.py
@@ -1,21 +1,13 @@
 import json
 import bisect
+import ast
 from pyspark import SparkContext, SQLContext
 
-# context initialization
-sc = SparkContext()
-sqlContext = SQLContext(sc)
+categories_dict_path = '/home/mrizzo/categories_dict.json'
+META_PATH = 'hdfs:///datasets/productGraph/metadata.json'
+OUTPUT_PATH = 'hdfs:///user/mrizzo/meta_df'
 
-categories_dict_path = '/home/cyu/categories_dict.json'
-metadata_path = 'hdfs:///datasets/productGraph/metadata.json'
-output_path = 'hdfs:///user/cyu/processed_metadata.parquet'
-
-# Load category dictionary from file
-categories_dict = None
-with open(categories_dict_path) as f:
-	categories_dict = sc.broadcast(json.load(f))
-
-price_bins = sc.broadcast([
+price_bins = [
     0,
     10,
     20,
@@ -30,56 +22,77 @@ price_bins = sc.broadcast([
     200,
     500,
     1000
-])
-
-def process(line):
-	row = eval(line)
-
-	# We don't want any reviews for which the price is either NaN or 0
-	# or which don't belong to any category
-	if 'categories' not in row:
-		return None
-	if ('price' not in row) or (row['price']==0):
-		return None
-
-	# price tier
-	row.update({'price_tier': bisect.bisect_left(price_bins.value, row['price'])})
-	
-	# main category
-	def get_main_category(categories):
-	
-		# flaten a list of lists
-		flat = set(sum(categories, []))
-	
-		# map category names, trim None
-		main_categories = filter(None, map(categories_dict.value.get, flat))
-
-		# uniquify, not neede if return only one category
-		#main_categories = list(set(main_categories))
-		
-		# return the first category, if exists
-		if len(main_categories) > 0:
-			return main_categories[0]
-		else:
-			return ''
-
-	row['main_category'] = get_main_category(row['categories'])
-	
-	# remove items for which there is no category
-	if row['main_category']:
-		return None
-	
-	return row
-
-# read the input file line by line, then load strings as jsons
-metadata = sc.textFile(metadata_path) \
-	.map(process) \
-	.filter(lambda x: x)
-
-# convert to dataframe
-metadata_df = sqlContext.createDataFrame(metadata)
-
-# save as parquet
-metadata_df.write.parquet(output_path)
+]
 
 
+def add_price_tier(row, bins):
+    # We don't want any reviews for which the price is either NaN or 0
+    # or which don't belong to any category
+    # if 'categories' not in row:
+    # return None
+    # if ('price' not in row) or (row['price']==0):
+    # return None
+
+    # price tier
+    row['price_tier'] = bisect.bisect_left(bins.value, row['price'])
+    return row
+
+
+def add_main_category(row, categories_dict):
+    # main category
+    def get_main_category(categories):
+
+        # flaten a list of lists
+        flat = set(sum(categories, []))
+
+        # map category names, trim None
+        main_categories = list(filter(None, map(categories_dict.value.get, flat)))
+
+        # uniquify, not needed if return only one category
+        # main_categories = list(set(main_categories))
+
+        # return the first category, if exists
+        if len(main_categories) > 0:
+            return main_categories[0]
+        else:
+            return ''
+
+    row['main_category'] = get_main_category(row['categories'])
+
+    # remove items for which there is no category
+    # if row['main_category']:
+    # return None
+
+    return row
+
+
+def main():
+    # context initialization
+    sc = SparkContext()
+    sqlContext = SQLContext(sc)
+    sqlContext.setConf('spark.sql.parquet.compression.codec', 'snappy')
+
+    bins = sc.broadcast(price_bins)
+
+    # Load category dictionary from file
+    with open(categories_dict_path) as f:
+        categories_dict = sc.broadcast(json.load(f))
+
+    # read the input file line by line, then load strings as jsons
+    metadata = sc.textFile(META_PATH) \
+        .map(ast.literal_eval) \
+        .map(lambda r: add_price_tier(r, bins)) \
+        .map(lambda r: add_main_category(r, categories_dict))
+    # .filter(lambda x: x)
+
+    # convert to dataframe
+    metadata_df = sqlContext.createDataFrame(metadata)
+
+    print('{} metadata rows'.format(metadata_df.count()))
+
+    # save as parquet
+    metadata_df.write.mode('overwrite').parquet(OUTPUT_PATH)
+
+
+if __name__ == '__main__':
+    main()
