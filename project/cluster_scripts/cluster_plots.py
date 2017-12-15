9414877f773ebf3eab68e8af5030ed11e631618a
Matteo Rizzo <32157960+matrizzo@users.noreply.github.com>
2017-12-15 23:10:37 +0100

    Update cluster plot script

--- project/cluster_scripts/cluster_plots.py
+++ project/cluster_scripts/cluster_plots.py
@@ -1,10 +1,51 @@
 import json
 import numpy as np
 from pyspark import SparkContext, SQLContext
-from pyspark.sql.functions import UserDefinedFunction
-from pyspark.sql.types import BooleanType
 
-DATA_PATH = 'hdfs:///user/mrizzo/filtered_df'
+NON_INCENT_PATH = 'hdfs:///user/mrizzo/non_incent_df'
+INCENT_PATH = 'hdfs:///user/mrizzo/incent_df'
+
+
+def get_plot_data(df, sqlContext, bins):
+    if 'data' in sqlContext.tableNames():
+        sqlContext.dropTempTable('data')
+
+    sqlContext.registerDataFrameAsTable(df, 'data')
+    queries = {
+        'avg_compound_by_rating':
+            'SELECT overall, AVG(compound_score) FROM data GROUP BY overall',
+        'avg_length_by_overall':
+            'SELECT overall, AVG(word_count) FROM data GROUP BY overall',
+        'avg_length_by_price_tier':
+            'SELECT price_tier, AVG(word_count) FROM data GROUP BY price_tier',
+        'avg_compound_by_category':
+            'SELECT main_category, AVG(compound_score) FROM data GROUP BY main_category',
+        'avg_length_by_category':
+            'SELECT main_category, AVG(word_count) FROM data GROUP BY main_category',
+        'num_reviews_by_overall':
+            'SELECT overall, COUNT(*) FROM data GROUP BY overall',
+        'num_reviews_by_price_tier':
+            'SELECT price_tier, COUNT(*) FROM data GROUP BY price_tier',
+        'num_reviews_by_category':
+            'SELECT main_category, COUNT(*) FROM data GROUP BY main_category'
+    }
+
+    ret = {
+        name: [row.asDict() for row in sqlContext.sql(query).collect()]
+        for (name, query) in queries.iteritems()
+    }
+
+    hist = [list(bins.value)]
+    for rating in xrange(1, 6):
+        hist.append(df.filter(df.overall == rating) \
+                    .select('compound_score') \
+                    .rdd \
+                    .histogram(list(bins.value))[1])
+
+    ret['sentiment_distribution_by_rating'] = hist
+
+    return ret
+
 
 def main():
     sc = SparkContext()
@@ -12,43 +53,22 @@ def main():
     sqlContext = SQLContext(sc)
     sqlContext.setConf('spark.sql.parquet.compression.codec', 'snappy')
 
-    df = sqlContext.read.parquet(DATA_PATH)
+    df_non_incent = sqlContext.read.parquet(NON_INCENT_PATH)
+    df_incent = sqlContext.read.parquet(INCENT_PATH)
 
-    bins = sc.broadcast(np.linspace(-1,1,11))
-    hist = [list(bins.value)]
-    for rating in range(1,6):
-        hist.append(df.filter(df.overall==rating) \
-                      .select('compound_score') \
-                      .rdd \
-                      .histogram(list(bins.value))[1])
-    with open('plots/sent_rating.txt','w') as f:
-        json.dump(hist, f)
+    non_incent_bootstap_samples = [df_non_incent.sample(True, 1.) for _ in xrange(10)]
+    incent_bootstap_samples = [df_incent.sample(True, 1.) for _ in xrange(10)]
 
-    sqlContext.registerDataFrameAsTable(df, "ALL_REVIEW")
-    rows = sqlContext.sql("SELECT overall, AVG(compound_score) FROM ALL_REVIEW GROUP BY overall").collect()
-    jsons = [row.asDict() for row in rows]
-    with open('plots/rating_compound.txt','w') as f:
-        json.dump(jsons, f)
+    bins = sc.broadcast(np.linspace(-1, 1, 11))
+    incent_results = [get_plot_data(s, sqlContext, bins) for s in incent_bootstap_samples]
+    non_incent_results = [get_plot_data(s, sqlContext, bins) for s in non_incent_bootstap_samples]
 
-    rows = sqlContext.sql("SELECT overall, AVG(word_count) FROM ALL_REVIEW GROUP BY overall").collect()
-    jsons = [row.asDict() for row in rows]
-    with open('plots/rating_length.txt','w') as f:
-        json.dump(jsons, f)
+    with open('plots/incent_results.json', 'w') as f:
+        json.dump(incent_results, f)
 
-    rows = sqlContext.sql("SELECT price_tier, AVG(word_count) FROM ALL_REVIEW GROUP BY price_tier").collect()
-    jsons = [row.asDict() for row in rows]
-    with open('plots/price_length.txt','w') as f:
-        json.dump(jsons, f)
+    with open('plots/non_incent_results.json', 'w') as f:
+        json.dump(non_incent_results, f)
 
-    rows = sqlContext.sql("SELECT main_category, AVG(compound_score) FROM ALL_REVIEW GROUP BY main_category").collect()
-    jsons = [row.asDict() for row in rows]
-    with open('plots/compound_category.txt','w') as f:
-        json.dump(jsons, f)
-
-    rows = sqlContext.sql("SELECT main_category, AVG(word_count) FROM ALL_REVIEW GROUP BY main_category").collect()
-    jsons = [row.asDict() for row in rows]
-    with open('plots/length_category.txt','w') as f:
-        json.dump(jsons, f)
 
 if __name__ == '__main__':
     main()
